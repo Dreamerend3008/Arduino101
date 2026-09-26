d6ce517195103106e27471c9c34b55265c62dee2
Dreamerend3008 <79518618+Dreamerend3008@users.noreply.github.com>
2026-09-26 11:09:37 -0500

    Mejora de comentarios

--- src/test/src/main.cpp
+++ src/test/src/main.cpp
@@ -1,31 +1,32 @@
 #include <Arduino.h>
 
-// lectura
+// Pins de lectura
 const uint8_t A_IN[] = {A5,A3,A0};
 const uint8_t D_IN[] = {4};
 
 
-// lectura valores
+// Valores de lectura 
 float l_temp;
 float l_micro;
 float l_pot;
 bool l_pul;
 
-// Salida
+// Pins de Salida
 const uint8_t rgb_led_OUT[] = {9,10,11};
 const uint8_t led_OUT[] = {3,5,6};
 const uint8_t buzerPin = 7;
 
-// variables normalizadas
-float temp;
-
-// variables de output
+// variables de salida 
 int rgb_led[] = {255,255,255};
 int led[3] = {255,255,255};
+
+// variables normalizadas
+float temp;
 
 
 void debug()
 {
+  // Funcion para debuggear las variables de lectura 
   Serial.print("l_temp (ADC): ");
   Serial.println(l_temp);
   Serial.print("temperatura: ");
@@ -41,10 +42,14 @@ void debug()
 }
 
 void fade_in_fade_out() {
+  // Funcion que hace que un led haga efecto fade in -> fade out
+
+  // Enciende el buzzer por 150ms
   digitalWrite(buzerPin, HIGH);
   delay(150);
   digitalWrite(buzerPin, LOW);
 
+  // Fade out
   for (int brightness = 255; brightness >= 0; brightness--) {
     for (int j = 0; j < 3; j++) {
       led[j] = brightness;
@@ -53,6 +58,7 @@ void fade_in_fade_out() {
     delay(2); 
   }
 
+  // Fade in 
   for (int brightness = 0; brightness <= 255; brightness++) {
     for (int j = 0; j < 3; j++) {
       led[j] = brightness;
@@ -63,14 +69,18 @@ void fade_in_fade_out() {
 }
 
 void emergency(){
-  // 1023, 0
+  // Funcion que hace que el led parpadee y suene el buzzer en un patron de SOS
+
+  // Normalizacion del tiempo de delay entre cada parpadeo, usando el valor del potenciómetro
   float time = map(l_pot, 0, 1023, 120, 500);
   digitalWrite(led_OUT[0], HIGH);
   digitalWrite(led_OUT[1], LOW);
   digitalWrite(led_OUT[2], LOW);
 
+  // Arreglo que contiene el patron de SOS, 1 = encendido, 0 = apagado
   bool sos[] = {1,1,1,0,0,0,1,1,1};
 
+  // Funcion que recorre sos, diferenciando entre los valores 1 y 0 para representar en codigo morse punto y raya
   for(int i = 0; i < 9; i++)
   {
     if(sos[i])
@@ -93,12 +103,11 @@ void emergency(){
     digitalWrite(led_OUT[2], LOW);
     delay(time);
   }
-  // 111000111
-
 }
 
 void setup()
 {
+  // Inicidalizamos los pines de lectura y salida
   pinMode(D_IN[0], INPUT_PULLUP);
   for(int i=0;i<3;i++)pinMode(led_OUT[i], OUTPUT);
   pinMode(buzerPin, OUTPUT);
@@ -116,32 +125,38 @@ void loop()
   l_pul = !digitalRead(D_IN[0]);
   temp = (l_temp * 5000.0 / 1024.0) / 10.0;
 
+  // Si la temperatura es menor a 25 grados, el led se pone naranja
   if(temp < 25)
   {
-    // orange
+    // Naranja 
     rgb_led[0] = 255;
     rgb_led[1] = 10;
     rgb_led[2] = 0;
 
   }
+  // Si la temperatura esta entre 25 y 35 grados, el led se pone morado
   else if(temp >= 25 && temp <= 35)
   {
-    // purple
+    // Morado 
     rgb_led[0] = 85;
     rgb_led[1] = 0;
     rgb_led[2] = 255;
   }
+  // Si la temperatura es mayor a 35 grados, el led se pone verde azulado
   else
   {
-    // verde azulado
+    // Verde azulado
     rgb_led[0] = 0;
     rgb_led[1] = 255;
     rgb_led[2] = 140;
-    if(l_pul) emergency();
+    // Si el boton esta presionado y la temperatura es mayor a 35 grados, se activa la funcion de emergencia
+    if(l_pul) emergency(); // La funcion de emergencia corresponde a la funcion con el patron creativo de encendido de leds
   }
 
   // output de variables
   for(int i=0;i<3;i++) analogWrite(rgb_led_OUT[i], rgb_led[i]);
+
+  // Si el valor del microfono es mayor a 800, se activa la funcion de fade in fade out
   if(l_micro > 800) for(int i=0;i<3;i++)fade_in_fade_out();
 
   for(int i=0;i<4;i++)analogWrite(led_OUT[i], 0);
